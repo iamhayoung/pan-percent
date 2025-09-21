0d327d0bfbc84e18051911cb50fc2f09870e8c37
iamhayoung <hahhyoung@gmail.com>
2025-09-22 01:30:38 +0900

    feat: crate not found screen

---
+++ app/+not-found.tsx
@@ -0,0 +1,21 @@
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Not found" }} />
+      <View style={styles.container}>
+        <Text>{"Oops! This screen doesn't exist."}</Text>
+        <Link href="/">Go to home screen</Link>
+      </View>
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
