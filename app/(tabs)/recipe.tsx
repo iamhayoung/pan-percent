4cecd53590fdd4dea5c8c83ec342670bb3c527c9
iamhayoung <hahhyoung@gmail.com>
2025-09-23 18:29:07 +0900

    feat: create CircleButton component for /recipe/register

--- app/(tabs)/recipe.tsx
+++ app/(tabs)/recipe.tsx
@@ -1,11 +1,25 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import CircleButton from "@/components/CircleButton";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function RecipesScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>recipe</Text>
-      <Link href="/recipe/register">Go to register</Link>
+
+      <View style={styles.circleButtonPosition}>
+        <CircleButton icon="add" href="/recipe/register" />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  circleButtonPosition: {
+    position: "absolute",
+    bottom: 24,
+    right: 24,
+  },
+});
